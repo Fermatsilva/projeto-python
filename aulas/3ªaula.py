a6f23d92c267004bc7e356507bcdc9c275fce585
fernandessilva.fgv@gmail.com <fernandessilva.fgv@gmail.com>
2022-07-17 16:36:46 -0300

    commit

---
+++ aulas/3ªaula.py
@@ -0,0 +1,9 @@
+if 5 > 2:
+  print("Cinco é maior do que dois")
+
+# No Python não é possível criar um bloco de comentários. Somente se for uma string multi line.
+
+"""
+Tudo que for colocado dentro dessas 3 aspas duplas será tratado como um bloco de comentário, pois é considerado uma string multiline, usada como bloco de comentário.
+"""
+
---
+++ aulas/4ªaula.py
@@ -0,0 +1,3 @@
+"""
+Váriáveis são espaços de memória criados pelo computador para armazenar informações na memória do computador.
+"""
